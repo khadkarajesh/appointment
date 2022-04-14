6b47551f8c2bd9fd8e19624466a8eaf8925e953e
Rajesh Khadka <rajesh.k.khadka@gmail.com>
2022-04-14 21:52:48 +0200

    add app factory

--- auth/resources/signup_resource.py
+++ auth/resources/signup_resource.py
@@ -1,17 +1,7 @@
-from flask import request
 from flask_restful import Resource
-
-from auth.managers.auth_manager import register_user
-from auth.schemas.user_schema import UserSchema
-from db import session
 
 
 class SignupResource(Resource):
     @classmethod
     def get(cls):
         return {'testing': 'get'}
-
-    @classmethod
-    def post(cls):
-        user = UserSchema().load(request.get_json(), session=session)
-        return register_user(user)
---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+from urllib.parse import quote
+
+
+class Config(object):
+    """Base config, uses staging database server."""
+    DEBUG = False
+    TESTING = False
+    DB_SERVER = '192.168.1.56'
+    DB_USERNAME = os.environ.get('DB_USER_NAME')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DB_DATABASE_NAME = os.environ.get('DB_NAME')
+    DB_PORT = os.environ.get('DB_PORT')
+
+    @property
+    def SQLALCHEMY_DATABASE_URI(self):
+        return f"postgresql://{self.DB_USERNAME}:{quote(self.DB_PASSWORD)}@{self.DB_SERVER}:{self.DB_PORT}/{self.DB_DATABASE_NAME}"
+
+
+class ProductionConfig(Config):
+    """Uses production database server."""
+    DB_SERVER = '192.168.19.32'
+
+
+class DevelopmentConfig(Config):
+    DB_SERVER = 'localhost'
+    DEBUG = True
+
+    def __str__(self):
+        print(f"{self.SQLALCHEMY_DATABASE_URI}")
+
+
+class TestingConfig(Config):
+    DB_SERVER = 'localhost'
+    DEBUG = True
+    DATABASE_URI = 'sqlite:///:memory:'
